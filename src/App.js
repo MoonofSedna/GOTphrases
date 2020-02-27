891dd98ced2783c59b6e8185f90d31de30091f2f
Sedna <60520922+MoonofSedna@users.noreply.github.com>
2020-02-26 22:26:41 -0500

    Version 2.0

--- src/App.js
+++ src/App.js
@@ -1,72 +1,76 @@
 import React, {useState, useEffect} from 'react';
 import styled from '@emotion/styled';
 import Phrase from "./components/Phrase";
+import imagen from "./logo192.png";
 
+  const Content = styled.div`
+    box-sizing: content-box;
+    display:flex;
+    align-items: center;
+    padding:3rem;
+    flex-direction: column;
+    margin-bottom:10rem;
 
-const Content = styled.div`
-  box-sizing: content-box;
-  display:flex;
-  align-items: center;
-  padding-top:10rem;
-  flex-direction: column;
+  `;
+  const Button = styled.button`
 
-`;
-const Button = styled.button`
+    background: transparent;
+    margin:auto;
+    background-size:  100px;
+    font-family: 'Fondamento';
+    color: #9a9a9a;
+    margin-top: 2rem;
+    padding: .6rem 2rem;
+    font-size: 1.2rem;
+    border: 1.5px solid #9a9a9a;
+    border-radius: 10px;
+    display:inline-block;
+    outline:none;
 
-  background: transparent;
-  background-size:  300px;
-  font-family: 'Fondamento';
-  color: #9a9a9a;
-  margin-top: 2rem;
-  padding: 1rem 3rem;
-  font-size: 1.5rem;
-  border: 1.5px solid #9a9a9a;
-  border-radius: 10px;
-  display:inline-block;
-  outline:none;
+    &:hover{
+      color: #fff;
+      border: 1.5px solid #fff;
+      cursor:pointer;
+      
 
-   &:hover{
-     color: #fff;
-     border: 1.5px solid #fff;
-     cursor:pointer;
+    }
+  `;
+
     
 
-   }
-`;
+  function App() {
 
-  
+      const [phrase, savePhrase] = useState({});
 
-function App() {
+      //****API ****//
 
-    const [phrase, savePhrase] = useState({});
+      const consultAPI= async () => {
+      const api = await fetch('https://got-quotes.herokuapp.com/quotes?');
+      const phrase = await api.json()
+      savePhrase(phrase);
+      
+    }
 
-    //****API ****//
+    //****Load a Phrase */
+    
+    useEffect ( () => {
 
-    const consultAPI= async () => {
-    const api = await fetch('https://got-quotes.herokuapp.com/quotes?');
-    const phrase = await api.json()
-    savePhrase(phrase);
-     
-   }
+      consultAPI();
 
-   //****Load a Phrase */
-   
-   useEffect ( () => {
+    }, []);
 
-    consultAPI();
-
-   }, []);
-
-  return (
-    <Content>
-
-    <Phrase 
-    phrase={phrase}/>
-    <Button onClick={consultAPI}>
-      Get phrase
-    </Button>
-   </Content>
-  );
-}
+    return (
+      <Content>
+        <div className="row">
+          <img src={imagen} alt="logo" className="ml-5"/>
+        </div> 
+      <Phrase 
+        phrase={phrase}/>
+      <Button onClick={consultAPI}>
+        Get phrase
+      </Button>
+    </Content>
+    );
+  }
 
 export default App;
--- src/components/Phrase.js
+++ src/components/Phrase.js
@@ -2,62 +2,62 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 
-const PhraseContend = styled.div `
-    padding: 3rem;
-    border-radius: .5rem;
-    max-width: 800px;
-    margin-top:10rem;
-    
+    const PhraseContend = styled.div `
+        padding: 0rem;
+        border-radius: .5rem;
+        max-width: 800px;
+        margin-top:2rem;
+        
 
-    @media(min-width: 992px){
-        margin-top:10rem;
-    }
+        @media(min-width: 992px){
+            margin-top:2rem;
+        }
 
-    h1{
-        font-family: Arial, Helvetica, sans-serif;
-        text-align:center;
-        font-size: 1.5em;
-        position: relative;
-        padding-left: 4rem;
-        color: #d6d6d6;
+        h1{
+            font-family: Arial, Helvetica, sans-serif;
+            text-align:center;
+            font-size: 1.3rem;
+            position: relative;
+            padding-left: 4rem;
+            color: #d6d6d6;
 
-        &:before{
-            content: open-quote;
-            font-size: 10rem;
+            &:before{
+                content: open-quote;
+                font-size: 6rem;
+                color: red;
+                margin-top:2rem;
+                position:absolute;
+                left: 0rem;
+                top: -4rem;
+
+            }
+            
+        }
+
+        p{
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-size:1.4rem;
+            font-weight: bold;
+            text-align:right;
             color: red;
-            position:absolute;
-            left: -1rem;
-            top: -2rem;
+            margin-top: 2rem;
 
         }
 
-        
+    `;
+
+
+    const Phrase = ({phrase}) => {
+
+        if(Object.keys(phrase).length === 0) return null;
+        return ( 
+
+            <PhraseContend>
+                <h1>{phrase.quote}</h1>
+                <p>{phrase.character}</p>
+            </PhraseContend>
+            
+        );
     }
-
-    p{
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        font-size:1.4rem;
-        font-weight: bold;
-        text-align:right;
-        color: red;
-        margin-top: 2rem;
-
-    }
-
-`;
-
-
-const Phrase = ({phrase}) => {
-
-    if(Object.keys(phrase).length === 0) return null;
-    return ( 
-
-        <PhraseContend>
-            <h1>{phrase.quote}</h1>
-            <p>{phrase.character}</p>
-        </PhraseContend>
-        
-     );
-}
  
 export default Phrase;
